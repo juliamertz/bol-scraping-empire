93b5a2f65b2f1454ef3c16279f8dfd44c984e737
Julia Mertz <info@juliamertz.dev>
2024-10-29 22:58:14 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,24 @@ use providers::{amazon, bol};
 use rust_xlsxwriter::Workbook;
 use std::io::{self, BufRead};
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    ask_location: bool,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Amazon,
+    Bol,
+}
+
 fn read_line(msg: &str) -> std::io::Result<String> {
     println!("{msg}:");
     let stdin = io::stdin().lock();
@@ -13,23 +31,22 @@ fn read_line(msg: &str) -> std::io::Result<String> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut args = std::env::args();
-    let subcommand = args.next().expect("a subcommand");
+    let cli = Cli::parse();
 
-    let url = read_line("Link naar amazon zoekresultaten")?;
-    let pages = read_line("Hoeveel paginas")?.parse().expect("Valid usize");
+    // let url = read_line("Link naar amazon zoekresultaten")?;
+    // let pages = read_line("Hoeveel paginas")?.parse().expect("Valid usize");
 
-    let products = match subcommand.to_lowercase().as_str() {
-        "bol" => bol::query_products(&url, pages).await?,
-        "amazon" => amazon::query_products(&url, pages).await?,
-
-        _ => anyhow::bail!("choose from: [bol, amazon]"),
-    };
-
-    let mut workbook = Workbook::new();
-    workbook.push_worksheet(products.as_worksheet()?);
-    workbook.save("products.xlsx")?;
-    println!("Done!");
+    // let products = match subcommand.to_lowercase().as_str() {
+    //     "bol" => bol::query_products(&url, pages).await?,
+    //     "amazon" => amazon::query_products(&url, pages).await?,
+    //
+    //     _ => anyhow::bail!("choose from: [bol, amazon]"),
+    // };
+    //
+    // let mut workbook = Workbook::new();
+    // workbook.push_worksheet(products.as_worksheet()?);
+    // workbook.save("products.xlsx")?;
+    // println!("Done!");
 
     Ok(())
 }
