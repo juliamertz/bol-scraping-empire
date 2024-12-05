eaa0b6920e075a66942b9e1044c6d5a8e0df785a
Julia Mertz <info@juliamertz.dev>
2024-12-05 21:43:20 +0100

    wip: rework cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,4 @@
-// mod config;
+mod config;
 use scraping::{self, providers::Provider, status::Status};
 #[cfg(feature = "updater")]
 mod versioning;
@@ -12,16 +12,25 @@ use std::{
     str::FromStr,
 };
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[arg(long)]
-    ask_location: bool,
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    #[arg(long)]
-    location: Option<PathBuf>,
+#[derive(Subcommand)]
+enum Commands {
+    Scrape {
+        #[arg(long)]
+        ask_location: bool,
+
+        #[arg(long)]
+        url: Option<String>,
+    },
+    Upload,
 }
 
 fn read_line(msg: &str) -> std::io::Result<String> {
@@ -35,16 +44,6 @@ static OUTFILE: &str = "products.xlsx";
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
-    // let conf = config::read()?;
-
-    let state = Status::new(|status| {
-        let mut stdout = io::stdout();
-        stdout.queue(cursor::MoveTo(0, 0)).expect("cursor to move");
-        stdout
-            .execute(terminal::Clear(terminal::ClearType::FromCursorDown))
-            .expect("to clear terminal");
-        write!(stdout, "{status}").expect("to write status into stdout");
-    });
 
     #[cfg(feature = "updater")]
     if let Err(err) = versioning::try_update().await {
@@ -54,7 +53,52 @@ async fn main() -> Result<()> {
         )
     }
 
-    let url = read_line("Link naar zoekresultaten")?;
+    match cli.command {
+        Commands::Scrape { url, ask_location } => handle_scrape_command(url, ask_location).await?,
+        Commands::Upload => handle_upload_command().await?,
+    };
+
+    Ok(())
+}
+
+async fn handle_upload_command() -> Result<()> {
+    use uploader::bol::{types::Offer, Client};
+
+    let conf = config::read()?;
+    let client = Client::new_with_session(&conf.bol).await?;
+
+    let offer = Offer::new(
+        "Comfort",
+        "9789059564169",
+        999.99,
+        10,
+        Some("REF12345"),
+    );
+
+    // let ser = serde_json::to_string_pretty(&offer)?;
+    // std::fs::write("./offer.json", ser)?;
+    client.create_offer(&offer).await?;
+
+    // client.get_orders().await?;
+
+    Ok(())
+}
+
+async fn handle_scrape_command(url: Option<String>, ask_location: bool) -> Result<()> {
+    dbg!(&url);
+    let state = Status::new(|status| {
+        let mut stdout = io::stdout();
+        stdout.queue(cursor::MoveTo(0, 0)).expect("cursor to move");
+        stdout
+            .execute(terminal::Clear(terminal::ClearType::FromCursorDown))
+            .expect("to clear terminal");
+        write!(stdout, "{status}").expect("to write status into stdout");
+    });
+
+    let url = match url {
+        Some(value) => value,
+        None => read_line("Link naar zoekresultaten")?,
+    };
     let pages = read_line("Hoeveel paginas? (1)")?.parse().unwrap_or(1);
 
     let provider = Provider::from_url(&url)?;
@@ -66,10 +110,7 @@ async fn main() -> Result<()> {
     println!("Output excel sheet gereed...");
 
     let mut outfile = PathBuf::from_str(OUTFILE).unwrap();
-    if let Some(path) = cli.location {
-        outfile = path;
-    } 
-    if cli.ask_location {
+    if ask_location {
         outfile = rfd::FileDialog::new()
             .set_file_name(OUTFILE)
             .save_file()
@@ -81,3 +122,4 @@ async fn main() -> Result<()> {
 
     Ok(())
 }
+
